04d4445e8da54d43e64624b23113b90876a99117
Ivan Kopenkov <i.kopenkov@corp.mail.ru>
2019-03-13 22:16:04 +0300

    first line rendered

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
+import { PointUtils } from 'src/utils/PointUtils/PointUtils';
+
+const xPointsRaw = [
+    1542412800000,
+    1542499200000,
+    1542585600000,
+    1542672000000,
+    1542758400000,
+    1542844800000,
+    1542931200000,
+    1543017600000,
+    1543104000000,
+];
+const yPointsRaw = [37, 20, 32, 39, 32, 35, 19, 65, 36];
+
+const xPercents = PointUtils.transformAbsPointsToPercents(xPointsRaw);
+const yPercents = PointUtils.transformAbsPointsToPercents(yPointsRaw);
+console.log({
+    xPercents,
+    yPercents,
+});
+
+const points = xPercents.map((x, index) => {
+    const y = yPercents[index];
+
+    return [x, y] as PointUtils.Point;
+});
+console.log(points);
+
+const pathDAttr = `M ${points.map(xy => xy.join(' ')).join(' L ')}`;
+console.log(pathDAttr);
+
 const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg.setAttribute('viewBox', '0 0 100 100');
 // svg.setAttribute('width', '500');
@@ -5,14 +37,20 @@ svg.setAttribute('viewBox', '0 0 100 100');
 
 // svg.style.width = '100%';
 // svg.style.height = '100%';
+const path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+path1.setAttribute('stroke', 'red');
+path1.setAttribute('stroke-width', '0.5');
+path1.setAttribute('fill', 'none');
+path1.setAttribute('d', pathDAttr);
+// <path id="lineAB" d="M 100 350 l 150 -300 l 150 -200" stroke="red" stroke-width="3" fill="none" />
 
-const cir1 = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-cir1.setAttribute('cx', '80');
-cir1.setAttribute('cy', '80');
-cir1.setAttribute('r', '30');
-cir1.setAttribute('fill', 'red');
+// const cir1 = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+// cir1.setAttribute('cx', '80');
+// cir1.setAttribute('cy', '80');
+// cir1.setAttribute('r', '30');
+// cir1.setAttribute('fill', 'red');
 
-svg.appendChild(cir1);
+svg.appendChild(path1);
 
 const containerEl = document.getElementsByClassName('svgWrapper')[0];
 
--- src/utils/PointUtils/PointUtils.ts
+++ src/utils/PointUtils/PointUtils.ts
@@ -1,19 +1,50 @@
+const PERCENTS = 100;
+
 export namespace PointUtils {
     export type Point = [number, number];
 
-    export const findExtremums = (points: Point[]) => {
-        const xMap = points.map(([x]) => x);
-        const yMap = points.map(([x, y]) => y);
+    export const getPointsOfAxis = (points: Point[], axis: 'x' | 'y') => {
+        return points.map(([x, y]) => ({ x, y }[axis]));
+    };
 
-        const xMin = Math.min(...xMap);
-        const xMax = Math.max(...xMap);
-        const yMin = Math.min(...yMap);
-        const yMax = Math.max(...yMap);
+    export const getPointsByAxis = (points: Point[]) => {
+        const xPoints = getPointsOfAxis(points, 'x');
+        const yPoints = getPointsOfAxis(points, 'y');
+        return {
+            xPoints,
+            yPoints,
+        };
+    };
+
+    export const findExtremums = (points: Point[]) => {
+        const { xPoints, yPoints } = getPointsByAxis(points);
+
+        const xMin = Math.min(...xPoints);
+        const xMax = Math.max(...xPoints);
+        const yMin = Math.min(...yPoints);
+        const yMax = Math.max(...yPoints);
         return {
             xMin,
             xMax,
             yMin,
             yMax,
         };
+    };
+
+    export const transformAbsPointsToPercents = (points: number[]) => {
+        const min = Math.min(...points);
+        const max = Math.max(...points);
+
+        const step = PERCENTS / (max - min);
+
+        const pointsShifted = points.map(point => point - min);
+
+        return pointsShifted.map(point => point * step);
+    };
+
+    export const pointsToPercents = (points: Point[], axis: 'x' | 'y') => {
+        // const points = getPointsOfAxis(points, axis);
+        // const { xPoints, yPoints } = getPointsByAxis(points);
+        // const { xMax, xMin, yMax, yMin } = findExtremums(points);
     };
 }
