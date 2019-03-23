b2502a0816086eca2d2a72a6e2df820158b56ac1
Ivan Kopenkov <i.kopenkov@corp.mail.ru>
2019-03-24 01:38:11 +0300

    enable source maps in production

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -2,29 +2,30 @@
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
-import TerserPlugin from 'terser-webpack-plugin';
+// import TerserPlugin from 'terser-webpack-plugin';
 import devConfig from './webpack.config';
 
 const config: webpack.Configuration = {
     mode: 'production',
     plugins: [new CleanWebpackPlugin()],
-    devtool: false,
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    mangle: {
-                        properties: true,
-                    },
-                    // ecma: 6,
-                    // module: true,
-                    // compress: {
-                    //     passes: 6,
-                    // },
-                },
-            }),
-        ],
-    },
+    devtool: 'source-map',
+    // optimization: {
+    //     minimizer: [
+    //         new TerserPlugin({
+    //             terserOptions: {
+    //                 // mangle: {
+    //                 //     properties: true,
+    //                 // },
+    //                 // sourceMap: true,
+    //                 // ecma: 6,
+    //                 // module: true,
+    //                 // compress: {
+    //                 //     passes: 6,
+    //                 // },
+    //             },
+    //         }),
+    //     ],
+    // },
 };
 
 export default merge(devConfig, config);
